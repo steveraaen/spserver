4929a2185681fb9544cd2c7f638001f52833913b
Steveraaen <sraaen@gmail.com>
2018-02-26 18:42:41 -0500

    added route to get ten nearest signs so user can marl position of their car

--- routes/dayRoutes.js
+++ routes/dayRoutes.js
@@ -59,6 +59,37 @@ module.exports = function(app) {
         }).limit(1500);        
     })
 
+    app.get("/mycar/:coordinates?", function(req, res) {
+        console.log(req.query)
+        if(req.query.day) {
+          var today = req.query.day  
+        } else {
+    var today = moment().format("dddd").toUpperCase()            
+        }
+       var d = today.substring(0, 3)
+        var lat = parseFloat(req.query.coordinates[1]).toFixed(6)
+        var lng = parseFloat(req.query.coordinates[0]).toFixed(6)
+       
+        var rte = new RegExp(".*^" + d + ".*")
+        signs.find({
+           /* "properties.T": rte,*/
+            geometry: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [lng, lat ]
+                    },
+                    $maxDistance: 1000
+                }
+            }
+        }, function(error, doc) {
+            if (error) {
+                console.log(error);
+            } else {             
+                res.json(doc);
+            }
+        }).limit(10);
+});
 
 
     
